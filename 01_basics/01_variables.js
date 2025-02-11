d04f233f78012c8d016db344d73ff20e004c8eea
Pranav Tiwari <159114564+pranav-jss@users.noreply.github.com>
2025-02-11 12:38:04 +0000

    variables & consts

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 144553;
+let accountEmail = "pt@pt.com";
+var accountPassword="abcde12";
+accountCity="Indore";
+
+// accountId="25658"; // not allowed
+
+accountEmail="gc.gc";
+accountPassword="123edc";
+accountCity="Pune";
+let accountState;
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+
+
