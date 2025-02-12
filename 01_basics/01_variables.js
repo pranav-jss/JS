8b3cc13aae11a7ada265a4e0fea3040c11227af1
Pranav Tiwari <159114564+pranav-jss@users.noreply.github.com>
2025-02-12 11:18:42 +0000

    small changes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,14 +1,17 @@
 const accountId = 144553;
+const accountName="Pranav";
 let accountEmail = "pt@pt.com";
 var accountPassword="abcde12";
 accountCity="Indore";
+
+console.log(accountName);
 
 // accountId="25658"; // not allowed
 
 accountEmail="gc.gc";
 accountPassword="123edc";
 accountCity="Pune";
-let accountState;
+let accountState;  //undefined will appear in output
 
 console.log(accountId);
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
