62cdcab3e18a28bdfd37c4a41b2994adbf88415e
Pranav Tiwari <159114564+pranav-jss@users.noreply.github.com>
2025-02-15 13:34:41 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -32,3 +32,45 @@ let someNumber = 33;
 
 let stringNumber = String(someNumber)
 console.log(stringNumber);
+
+//------------------------**Operations**----------------------------------//
+
+let value = 5;
+let negValue = -value;
+console.log(negValue);
+
+console.log(2+2); // output will be 4
+console.log(2*2); // output will be 4
+console.log(2**3); // output will be 8 (2 raise to the power 3)
+console.log(2-2); // putput will be 0
+console.log(2/2); //output will be 1
+console.log(2%3); // (to find remainder) output will be 2
+
+let str1 = "hello";
+let str2 = " pranav";
+let str3 = str1 + str2;
+console.log(str3);
+
+//confusing part:
+
+console.log(1 + 2);  //output is 3
+console.log("1" + 2); //output is 12
+console.log(1 + "2"); //output is 12
+console.log("1" + 2 + 2);  //output is 122
+console.log(1 + 2 + "2"); //output is 32
+
+//
+
+console.log(true);  //output is true
+console.log(+true);  //output is 1
+console.log(+""); //output is 0
+
+
+let num1, num2, num3;
+num1 = num2= num3 = 2+2;  // don't write such complex code, code readibility should be high
+
+let gameCounter = 100;
+//gameCounter++;   //postfix operator
+++gameCounter;     //prefix operator
+console.log(gameCounter);  //output is 101
+
