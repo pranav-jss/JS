7a9c02e262b41f7dce57ca12a4b3e2aba0ba0cfd
Pranav Tiwari <159114564+pranav-jss@users.noreply.github.com>
2025-02-15 16:05:10 +0000

    stack and heap memory

--- 01_basics/datatypeSummary.js
+++ 01_basics/datatypeSummary.js
@@ -39,4 +39,39 @@ console.log(typeof myFunction);
 console.log(typeof id);
 console.log(typeof superheros); //typeof is object
 
+//.........................................................
 
+//Memory : Stack memory and heap memory
+// stack memory and heap memory
+// stack memory (used in primitive datatypes) (change in copy)
+//heap memory (used in non-primitive datatypes) (change in original value)
+
+let myYoutubeName = "codingdotcom";
+let anotherYoutube = myYoutubeName;
+console.log(anotherYoutube);
+console.log(myYoutubeName);
+
+anotherYoutube = "finalcodedotcom"
+console.log(myYoutubeName);
+console.log(anotherYoutube);
+
+//in above example there we get a copy from myYoutubeName and the changes happen in anotherYoutube will be changed in the copy and not in its original value/memory
+
+//heap
+
+let user1 = {
+    name: "pranav",
+    email: "pratgmaildotcom",
+}
+
+let user2 = user1;
+
+console.log(user1.email);
+console.log(user2.email);
+
+user2.email="propdotcom";
+
+console.log(user1.email);
+console.log(user2.email);
+
+//changes happen in heap or user2 will happen in user1 also
